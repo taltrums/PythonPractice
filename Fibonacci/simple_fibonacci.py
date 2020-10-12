deaec3baa9147d7ec10ee199413de22d814aa8e2
sidecodes <sidekicktal@outlook.com>
2020-10-13 00:59:25 +0530

    Simple Fibonacci

---
+++ Fibonacci/simple_fibonacci.py
@@ -0,0 +1,9 @@
+# simple fibonacci series
+# the sum of two elements defines the next set
+
+a, b = 0, 1
+while b < 1000:
+    print(b, end = ' ', flush = True)
+    a, b = b, a + b
+
+print() # line ending
