fa46d3011813b39396ebb037297b2feb5ab69e48
sidecodes <sidekicktal@outlook.com>
2020-10-13 00:45:58 +0530

    Dictionaries Example

---
+++ DS/dictionaries.py
@@ -0,0 +1,10 @@
+# Key: State
+# Value: Capital
+
+statesToCapitals = {}
+
+statesToCapitals["Germany"] = "Berlin"
+statesToCapitals["New York"] = "Albany"
+
+print(statesToCapitals["New York"])
+print(statesToCapitals["Germany"])
